11b45e47198c40b4496f7c7e3a56c79617eb4adc
Chano <136133749+chanovera-dev@users.noreply.github.com>
2024-04-19 14:57:15 -0600

    ejercicio 0

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/chanovera-dev.py
@@ -0,0 +1,35 @@
+# web oficial de python: https://python.org
+
+# esto es un comentario de una línea
+
+"""
+esto es un comentario
+de varias líneas
+"""
+
+'''
+esto es un comentario
+de varias líneas
+'''
+
+mi_variable = "esto es una variable de cadena de texto"
+
+# python no tiene constantes, pero:
+MI_CONSTANTE = "es variable, pero se pone en mayúsculas para que sepan que no deben cambiar este valor"
+
+# int es entero
+mi_entero_1 = 1
+
+#  float es decimal
+mi_flotante_1 = 1.1
+
+# booleanos
+mi_booleano = true
+mi_booleano = false
+
+# strings o cadenas de texto
+mi_string1 = "esto es una cadena de texto"
+mi_string2 = 'esto también es una cadena de texto'
+
+# imprimir un hola mundo
+print("hola mundo")
